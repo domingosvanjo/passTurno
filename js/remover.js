e5239bab285f528332473324dbf65e522a079e5a
Domingos Vanjo <92234851+domingosvanjo@users.noreply.github.com>
2023-01-02 00:07:10 -0300

    Update remover.js

--- js/remover.js
+++ js/remover.js
@@ -5,7 +5,7 @@ botaoRemover.addEventListener("dblclick", function(event){
     var alvoEvento = event.target;
     var paiDoAlvo = alvoEvento.parentNode; // Tr = paciente = remover
     var avoDoAlvo = paiDoAlvo.parentNode;
-    avoDoAlvo.parentNode.classList.add("fadeOut");
+    paiDoAlvo.parentNode.classList.add("fadeOut");
     alert(avoDoAlvo);
     setTimeout(function(){
         avoDoAlvo.remove();
